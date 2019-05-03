dfe3ec5c33b70149785fd6e15b2e0103105afd6c
Filipp <filippstryukov@gmail.com>
2019-05-03 20:45:12 +0300

    Добавил внутрение и главный массивы

---
+++ JavaScriptArray.js
@@ -0,0 +1,8 @@
+//Добавил внутрение массивы
+var arr_1 = new Array("Wow" , "First" , "Array");
+var arr_2 = new Array("WOw" , "Second" , "Array");
+var arr_3 = new Array("WOW" , "Third" , "Array");
+
+//Добавил массив
+var arr = new Array(arr_1 , arr_2 , arr_3);
+
