1c096a14d1bdc420d90ecc603f7c2495bfd60a27
Filipp <filippstryukov@gmail.com>
2019-05-03 21:24:55 +0300

    Add code(don't work)

--- JavaScriptArray.js
+++ JavaScriptArray.js
@@ -6,3 +6,40 @@ var arr_3 = new Array("WOW" , "Third" , "Array");
 //Добавил массив
 var arr = new Array(arr_1 , arr_2 , arr_3);
 
+//Функция (принимает только строковые аргументы!)
+function ShowElements(arg)
+{
+
+/*
+    var y;
+    var elementsArray = 0;
+    
+    document.write(arg);
+
+    Проверка , есть ли в массиве внутренние массивы
+    for(y = 0; y < arg.lenght; y++){
+
+        elementsArray += y; 
+
+    }
+    if(elementsArray == 0){document.write("В массиве" + arg + " нет внутренних массивов")}*/
+    
+
+        //Нахождение всех элементов внутренних массивов
+        for( var i = 0; i < arg.lenght; i++){
+
+            for( var x = 0; x < arg[i].lenght; x++){
+
+            document.write(arg[i][x] + "\n");
+
+            }
+
+        document.write( "\n" );
+
+        }
+
+    }
+
+
+    ShowElements(arr);
+    
