d15d266f9ea11a70934fba26f0fb131fd726cf60
Filipp <filippstryukov@gmail.com>
2019-05-04 00:16:46 +0300

    Edit js code

--- JavaScriptArray.js
+++ JavaScriptArray.js
@@ -1,5 +1,5 @@
  function sortFunction(a,b) {
-        return a-b;
+        return b-a;
     }
 //Добавил внутрение массивы
 var arr_1 = new Array("Wow" , "First" , "Array");
@@ -8,6 +8,7 @@ var arr_3 = new Array("WOW" , "Third" , "Array");
 var arr_clear = new Array();
 
 //Добавил массив
+var arr_With_num = new Array( 9 , 6 , 7);
 var arr_Clear = new Array();
 var arr = new Array(arr_1 , arr_2 , arr_3);
 //Массив без внутренностей
@@ -16,20 +17,23 @@ var arr = new Array(arr_1 , arr_2 , arr_3);
 
 
 
+
+
 function ShowElements(arg){
   
-    var summ = 0;
-
+    /*var summ = 0;*/
+   
     //Проверка , есть ли в массиве внутренние массивы
     for(var y = 0; y < arg.length; y++){
      
         for(var t = 0; t < arg[y].length; t++){
     	
-        summ += t;
 
         }
 
     }
+
+
 
     //Проверка и вычисление , на сумму всех элементов массива , если элементы = Number (Не работает!)
     /*for(var y = 0; y < arg.length; y++){
@@ -67,7 +71,7 @@ function ShowElements(arg){
 
         for( var x = 0; x < arg[i].length; x++){
 
-            document.write(arg[i][x]);
+            document.write(arg[i][x] + " ");
 
             }
 
@@ -82,7 +86,7 @@ function ShowElements(arg){
 
 
 } 
-ShowElements(arr_Clear);
+ShowElements(arr);
 
 
 
