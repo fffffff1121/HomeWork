6785c75ebb3cbf1ab71571321f43ee21ba07d300
Filipp <filippstryukov@gmail.com>
2019-05-06 22:05:43 +0300

    Update the code

--- JavaScriptArray.js
+++ JavaScriptArray.js
@@ -1,5 +1,33 @@
-var arrWithNum = [2, 6, 7, 9];
+function sortMinToMax(a, b) {
+	return a - b;
+}
 
-arrWithNum.forEach(function(item, i) {
-    document.write(item + "<br/>");
-});
+var arr_1 = [2, 6, 4, 9];
+var arr_2 = [7, 4, 2, 10];
+var arr_3 = [99, 1, 3, 5];
+
+var arr_4 = ["Nice", "Click"];
+var arr_5 = ["GG", "Coder"];
+var arr_6 = ["This", "Is", "Array"];
+
+
+var arrWithNum = [arr_1, arr_2, arr_3];
+
+var arrWithLetter = [arr_4, arr_5, arr_6]
+
+function readArr(arr) {
+
+	arr.forEach(function(item, i) {
+		if (!isNaN(arr[i][i])) {
+			arr[i].sort(sortMinToMax);
+		}
+		document.write(item + "<br/>");
+	});
+
+
+
+}
+
+readArr(arrWithNum);
+
+readArr(arrWithLetter);
